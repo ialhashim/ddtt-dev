05fa15e6b0e1a876f386a9e3066a4f3c52d9a094
JJCAO <jjcao1231@gmail.com>
2015-01-08 00:51:33 -0800

    several untracked files are added.

---
+++ ddtt/experiment/SymRelationViz/PointCloud.cpp
@@ -0,0 +1,41 @@
+#include "PointCloud.h"
+#include <qfile.h>
+
+PointCloud::PointCloud()
+{
+}
+
+
+PointCloud::~PointCloud()
+{
+}
+
+void PointCloud::load(const QString &filename)
+{
+	QFile file(filename);
+	if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+	{
+		QString str("can't open ");
+		str.append(filename);
+		QMessageBox::warning(0, "Warnning", str, QMessageBox::Yes);
+		return;
+	}
+	m_points.clear(); m_normals.clear();
+
+	QTextStream in(&file);
+	QString str;
+	for (int i = 0; i < 4; ++i)
+	{
+		str = in.readLine();
+	}
+	float x,y,z,nx,ny,nz;
+	while (!in.atEnd())
+	{
+		in >> x >> y >> z >> nx >> ny >> nz;
+		m_points.push_back(Surface_mesh::Point(x, y, z));
+		m_normals.push_back(Surface_mesh::Point(nx, ny, nz));
+	}
+
+
+	file.close();
+}
---
+++ ddtt/experiment/SymRelationViz/QuickPointsDraw.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "PointCloud.h"
+#include <qgl.h>
+
+#define glVertQt(v) glVertex3d(v.x(), v.y(), v.z())
+#define glColorQt(c) glColor4d(c.redF(), c.greenF(), c.blueF(), c.alphaF())
+
+#include <time.h>
+static std::vector<double> randomColor()
+{
+	std::vector<double> color;
+
+	float r = float(qMin(((rand() % 225) + 30), 255)) / 255.0f;
+	float g = float(qMin(((rand() % 230) + 25), 255)) / 255.0f;
+	float b = float(qMin(((rand() % 235) + 20), 255)) / 255.0f;
+
+	color.push_back(r);
+	color.push_back(g);
+	color.push_back(b);
+	color.push_back(1.0);
+
+	return color;
+}
+
+static std::vector< std::vector<double> > randomColors(int count)
+{
+	srand(time(NULL));
+
+	std::vector< std::vector<double> > colors(count);
+	for (int i = 0; i < count; i++)
+		colors[i] = randomColor();
+	return colors;
+}
+
+static QColor qRandomColor()
+{
+	std::vector<double> c = randomColor();
+	return QColor::fromRgbF(c[0], c[1], c[2], c[3]);
+}
+
+struct QuickPointsDraw
+{
+	static void drawPoints(PointCloud * pts, float size = 1.0, QColor c = QColor(255, 255, 255, 255))
+	{
+		if (!pts) return;
+
+		glPointSize(size);
+		glColorQt(c);
+		glDisable(GL_LIGHTING);
+		if (pts->hasNormals())
+		{
+			glEnable(GL_LIGHTING);
+			glBegin(GL_POINTS);
+			for (int i = 0; i < pts->size(); ++i)
+			{
+				SurfaceMesh::Point& n = pts->normals(i);
+				glNormal3d(n.x(), n.y(), n.z());
+				glVertQt(pts->points(i));
+			}
+			glEnd();
+		}
+		else
+		{
+			glBegin(GL_POINTS);
+			for (int i = 0; i < pts->size(); ++i)
+			{
+				glVertQt(pts->points(i));
+			}
+			glEnd();
+		}
+
+		glEnable(GL_LIGHTING);
+	}
+};
